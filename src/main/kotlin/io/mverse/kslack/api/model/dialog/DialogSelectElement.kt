8ef990ffc5c60cfc58c015914acb71d037f30299
BuildTools <unconfigured@null.spigotmc.org>
2018-12-17 12:35:31 -0700

    Adding datasource

--- src/main/kotlin/io/mverse/kslack/api/model/dialog/DialogSelectElement.kt
+++ src/main/kotlin/io/mverse/kslack/api/model/dialog/DialogSelectElement.kt
@@ -1,5 +1,7 @@
 package io.mverse.kslack.api.model.dialog
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Represents a [select](https://api.slack.com/dialogs#select_elements)
  * dialog element
@@ -13,42 +15,57 @@ package io.mverse.kslack.api.model.dialog
  */
 data class DialogSelectElement(
 
-  /**
-   * Label displayed to user. Required. No more than 24 characters.
-   */
-  override val label: String,
+    /**
+     * Label displayed to user. Required. No more than 24 characters.
+     */
+    override val label: String,
 
-  /**
-   * Name of form element. Required. No more than 300 characters.
-   */
-  override val name: String,
+    /**
+     * Name of form element. Required. No more than 300 characters.
+     */
+    override val name: String,
 
-  /**
-   * Type of element.  For a dropdown (select), the type is always
-   * `select` . It's required.
-   *
-   * @see [Dialog form elements](https://api.slack.com/dialogs.elements)
-   */
-  override val type: String = "select",
+    /**
+     * Type of element.  For a dropdown (select), the type is always
+     * `select` . It's required.
+     *
+     * @see [Dialog form elements](https://api.slack.com/dialogs.elements)
+     */
+    override val type: String = "select",
 
-  /**
-   * A default value for this field.  Must match a value presented in [DialogOption]s.
-   */
-  override val value: String,
+    /**
+     * A default value for this field.  Must match a value presented in [DialogOption]s.
+     */
+    override val value: String,
 
-  /**
-   * A string displayed as needed to help guide users in completing the element.
-   * 150 character maximum.
-   */
-  override val placeholder: String,
+    /**
+     * A string displayed as needed to help guide users in completing the element.
+     * 150 character maximum.
+     */
+    override val placeholder: String,
 
-  /**
-   * Provide true when the form element is not required. By default, form elements are
-   * required.
-   */
-  override val isOptional: Boolean = false,
+    /**
+     * Provide true when the form element is not required. By default, form elements are
+     * required.
+     */
+    override val isOptional: Boolean = false,
 
-  /**
-   * Provide up to 100 option element attributes. Required for this type.
-   */
-  val options: List<DialogOption>? = null) : DialogElement
+    val dataSource: Datasource = Datasource.STATIC,
+    val minQueryLength: Int? = null,
+
+    /**
+     * Provide up to 100 option element attributes. Required for this type.
+     */
+    val options: List<DialogOption>? = null) : DialogElement {
+
+  enum class Datasource {
+    @SerializedName("static") STATIC,
+    @SerializedName("external") EXTERNAL,
+    @SerializedName("users") USERS,
+    @SerializedName("channels") CHANNELS,
+    @SerializedName("conversations") CONVERSATIONS;
+
+    val value: String = name.toLowerCase()
+    override fun toString(): String = value
+  }
+}
